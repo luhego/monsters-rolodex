e757d2599ba32e860436ab940c12211e8c5e41c5
Luis Herrera <luhegoes@gmail.com>
2020-06-11 01:46:59 -0500

    Updates

---
+++ precache-manifest.e5d37f70c5ebdcb249c91ad751806bd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d6527ac0a4f20916093199be4e82a5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3f179a4635b474916482",
+    "url": "/monsters-rolodex/static/css/main.be77a3fc.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f179a4635b474916482",
+    "url": "/monsters-rolodex/static/js/main.641550ea.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
